c827b2b990ea8c0678abb22b2ac70411720beb02
benjimor <benji_moreau@hotmail.fr>
2020-01-30 13:43:03 +0100

    use pyyaml to parse the mapping

--- program2.py
+++ program2.py
@@ -7,18 +7,19 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
-stream=open(sys.argv[1])
+stream = open(sys.argv[1])
 
-data = load(stream, Loader=Loader)
-output = dump(data, Dumper=Dumper)
 
-mot=output.split( '\n' )
-#print(mot)
+yarrrml_mapping = load(stream, Loader=Loader)
+# output = dump(data, Dumper=Dumper)
+
+# mot=output.split( '\n' )
 
 
 liste_properties = []
 liste_classes = []
-#les_autres = []
+# les_autres = []
+'''
 for m in mot:
     if "[a," in m:
         n = re.search(r"a, '(.+?)']", m)
@@ -31,3 +32,15 @@ for m in mot:
             #..  else:
             # les_autres.append(m)
             # print(len(liste_properties))
+'''
+
+for mapping_name, mapping in yarrrml_mapping['mappings'].items():
+    # for each mapping
+    print(mapping_name)
+    predicate_objects = mapping['predicateobjects']
+    for predicate_object in predicate_objects:
+        # for each predicate object (list) in mapping
+        print(predicate_object)
+
+print(liste_classes)
+print(liste_properties)
