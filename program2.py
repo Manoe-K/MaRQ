6b34a7b7c2a43ee2dd2477f98c5b3623a0dd5a0f
fatim <fatim@gmail.com>
2020-01-31 14:17:14 +0100

    Support all YARRRML keys

--- program2.py
+++ program2.py
@@ -1,10 +1,24 @@
 import argparse
 
 from yaml import load
+
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
 except ImportError:
     from yaml import Loader, Dumper
+
+parser = argparse.ArgumentParser(description='Find federated queries for a federation.')
+parser.add_argument('mapping', type=str, help='yarrrml mapping filepath')
+args = parser.parse_args()
+
+# open yarrrml file
+stream = open(args.mapping)
+
+
+
+
+# loading the text file
+yarrrml_mapping = load(stream, Loader=Loader)
 
 YARRRML_KEYS = {
     'mappings': ['mappings', 'mapping'],
@@ -14,32 +28,50 @@ YARRRML_KEYS = {
     'value': ['value', 'v']
 }
 
-parser = argparse.ArgumentParser(description='Find federated queries for a federation.')
-parser.add_argument('mapping', type=str, help='yarrrml mapping filepath')
-args = parser.parse_args()
 
-stream = open(args.mapping)
+def get_keys(d, key):
+    # Get the value of the first key in corresponding YARRRML_KEYS that match a key in d
+    if key in YARRRML_KEYS:
+        for yarrrml_key in YARRRML_KEYS[key]:
+            if yarrrml_key in d:
+                return d[key]
+    return {}
 
-yarrrml_mapping = load(stream, Loader=Loader)
-
-print(yarrrml_mapping)
 
 liste_properties = []
 liste_classes = []
-# les_autres = []
 
-for mapping_name, mapping in yarrrml_mapping['mappings'].items():
-    # for each mapping
-    print(mapping_name)
-    predicate_objects = mapping['predicateobjects']
-    for predicate_object in predicate_objects:
-        # for each predicate object (list) in mapping
-        print(predicate_object)
-        if predicate_object[0] == 'a':
-            print(predicate_object[0])
 
-print(liste_classes)
-print(liste_properties)
+#def mapping_list(yarrrml):
+# get  yarrrml key of the yarrrml file
+key = list(yarrrml_mapping.keys())[0]
 
-test = {'classes': liste_classes,
-        'properties:': liste_properties}
+liste = get_keys(yarrrml_mapping, key)
+for mapping_name, mapping in liste.items():
+        # for each mapping
+        # print(mapping_name)
+        predicate_objects = mapping[list(mapping.keys())[0]]
+
+        for predicate_object in predicate_objects:
+            # for each predicate object (list) in mapping
+            # print(predicate_object)
+
+            if predicate_object[0] == 'a' or predicate_object[0] == 'rdf:type':
+                liste_classes.append(predicate_object[1])
+            elif len(predicate_object) == 3:
+                liste_properties.append(predicate_object[0])
+                liste_properties.append(predicate_object[2])
+            else:
+                liste_properties.append(predicate_object[0])
+
+print({'classes': liste_classes, 'properties:': liste_properties})
+
+
+
+
+#method test
+#print(mapping_list(yarrrml_mapping))
+
+
+# test = {'classes': liste_classes,
+#  'properties:': liste_properties}
