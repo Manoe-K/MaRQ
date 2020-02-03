bb289ea8b7f7c02569475e53ae37810bb3b2f483
fatim <fatim@gmail.com>
2020-02-03 11:04:39 +0100

    function compare

--- program2.py
+++ program2.py
@@ -46,17 +46,61 @@ def mapping_list(yarrrml):
         # for each mapping
         datasource = source[0]
     return {'classes': classes,
-            'properties:': properties,
+            'properties': properties,
             'dataset': datasource}
+
+
+
+def mapping_compare(yarrrml_map1, yarrrml_map2):
+
+    classes = []
+    properties = []
+
+    mapping1 = mapping_list(yarrrml_map1)
+    mapping2 = mapping_list(yarrrml_map2)
+
+    for mapping_name, mapping in mapping1.items():
+        # for each mapping
+        classesM1 =mapping1['classes']
+        propertiesM1 = mapping1['properties']
+
+    for mapping_name, mapping in mapping2.items():
+    # for each mapping
+        classesM2 = mapping2['classes']
+        propertiesM2 = mapping2['properties']
+
+    for classes2 in classesM2:
+            for classes1 in classesM1:
+                if classes2==classes1:
+                    classes.append(classes2)
+
+
+
+
+    for properties2 in propertiesM2:
+            for properties1 in propertiesM1:
+                if properties2==properties1:
+                    properties.append(properties2)
+
+
+    return {'classes': classes,
+            'properties:': properties}
+
 
 
 parser = argparse.ArgumentParser(description='Find federated queries for a federation.')
 parser.add_argument('mapping', type=str, help='yarrrml mapping filepath')
+parser.add_argument('mapping2', type=str, help='yarrrml mapping filepath')
+
 args = parser.parse_args()
+
 # open yarrrml file
 stream = open(args.mapping)
+stream2 = open(args.mapping2)
+
 # loading the text file
 yarrrml_mapping = load(stream, Loader=Loader)
+yarrrml_mapping2 = load(stream2, Loader=Loader)
 # method test
-print(mapping_list(yarrrml_mapping))
+print(mapping_compare(yarrrml_mapping, yarrrml_mapping2))
 
